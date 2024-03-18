f086efd895b15295778c791524b097e838fc6675
Aayush Anand <121629475+aayush751@users.noreply.github.com>
2024-03-18 13:59:23 +0530

    Time: 266 ms (41.01%) | Memory: 93.1 MB (95.79%) - LeetSync

---
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>>& points) {
+        int ans = 1;
+
+        sort( points.begin(), points.end() );
+        // sort esliye kr rhe taki overlapping point mil sake smjhe noob stalker
+
+        int temp = points[0][1];
+
+        for(int i = 1; i < points.size(); i++ )
+        {
+            if( points[i][0] > temp )
+            {
+                ans++;
+                temp = points[i][1];
+            }
+            else
+            {
+                temp = min( temp, points[i][1] );
+            }
+        }
+
+        return ans;
+    }
+};
