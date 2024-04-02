158ed22c3fba003e6cf0959167bfeb930863a526
Aayush Anand <121629475+aayush751@users.noreply.github.com>
2024-04-02 08:44:37 +0530

    Time: 4 ms (78.16%) | Memory: 9.4 MB (42.66%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map< char, int > m,n;
+
+        for(int i = 0; i < s.size(); i++ )
+        {
+            m[s[i]] = i;
+            n[t[i]] = i;
+        }
+
+        for(int i = 0; i < s.size(); i++ )
+        {
+            if( m[s[i]] != n[t[i]] )
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
