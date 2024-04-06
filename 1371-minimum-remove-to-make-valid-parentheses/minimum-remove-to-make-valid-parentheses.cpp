4a9c30d7bc48b17a7ae0a062aacd3a39a701466b
Aayush Anand <121629475+aayush751@users.noreply.github.com>
2024-04-06 22:50:29 +0530

    Time: 25 ms (31.31%) | Memory: 13 MB (51.14%) - LeetSync

---
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        // PHLE STACK se ban rhe hain
+
+
+        stack< int > st;
+
+        for(int i = 0; i < s.size(); i++ )
+        {
+            if( s[i] == '(' )
+            {
+                st.push( i );
+            }
+            else if( s[i] == ')' )
+            {
+                if( st.empty() )
+                {
+                    s[i] = '$';
+                }
+                else
+                {
+                    st.pop();
+                }
+            }
+        }
+
+        while( !st.empty() )
+        {
+            s[st.top()] = '$';
+            st.pop();
+        }
+
+        string ans = "";
+
+        for(int i = 0; i < s.size(); i++ )
+        {
+            if( s[i] != '$' )
+            {
+                ans += s[i];
+            }
+        }
+
+        return ans;
+    }
+};
