c54477c6b3f99285de7ad3c9ae9738ca616ac872
Aayush Anand <121629475+aayush751@users.noreply.github.com>
2024-04-06 23:00:18 +0530

    Time: 21 ms (57.18%) | Memory: 12.2 MB (79.62%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
@@ -4,32 +4,88 @@ public:
         // PHLE STACK se ban rhe hain
 
 
-        stack< int > st;
+        // stack< int > st;
+
+        // for(int i = 0; i < s.size(); i++ )
+        // {
+        //     if( s[i] == '(' )
+        //     {
+        //         st.push( i );
+        //     }
+        //     else if( s[i] == ')' )
+        //     {
+        //         if( st.empty() )
+        //         {
+        //             s[i] = '$';
+        //         }
+        //         else
+        //         {
+        //             st.pop();
+        //         }
+        //     }
+        // }
+
+        // while( !st.empty() )
+        // {
+        //     s[st.top()] = '$';
+        //     st.pop();
+        // }
+
+        // string ans = "";
+
+        // for(int i = 0; i < s.size(); i++ )
+        // {
+        //     if( s[i] != '$' )
+        //     {
+        //         ans += s[i];
+        //     }
+        // }
+        
+
+        // ab bina stack lekr solve kr rhe hain
+
+        int cnt = 0;
 
         for(int i = 0; i < s.size(); i++ )
         {
             if( s[i] == '(' )
             {
-                st.push( i );
+                cnt++;
             }
             else if( s[i] == ')' )
             {
-                if( st.empty() )
+                if( cnt == 0 )
+                {
+                    s[i] = '$';                 
+                }
+                else
+                {
+                    cnt--;
+                }
+            }
+        }
+
+        cnt = 0;
+
+        for(int i = s.size() - 1; i > -1; i-- )
+        {
+            if( s[i] == ')' )
+            {
+                cnt++;
+            }
+            else if( s[i] == '(' )
+            {
+                if( cnt == 0 )
                 {
                     s[i] = '$';
                 }
                 else
                 {
-                    st.pop();
+                    cnt--;
                 }
             }
         }
 
-        while( !st.empty() )
-        {
-            s[st.top()] = '$';
-            st.pop();
-        }
 
         string ans = "";
 
