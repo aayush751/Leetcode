f221dc13889bcfe445d554d0d376579ae0516a75
Aayush Anand <121629475+aayush751@users.noreply.github.com>
2024-04-11 22:40:59 +0530

    Time: 9 ms (72.26%) | Memory: 9.9 MB (60.50%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.cpp
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -1,38 +1,47 @@
-#include <bits/stdc++.h>
-#include <string>
-#include <stack>
-using namespace std;
-
-class Solution
-{
+class Solution {
 public:
-    string removeKdigits(string num, int k)
-    {
-        stack<char> st;
-        for (int i = 0; i < num.size(); i++)
+    string removeKdigits(string num, int k) {
+        // esme dip dekhna hai....
+        //  jse ki  current element se bada agar koe aata hai toh stack me push kro...agr chhota aata hai toh remove kr stack k top element ko and chhota value ko insert kro stack me. 
+
+        stack< char > st;
+
+        for(int i = 0; i < num.size(); i++ )
         {
-            while (k > 0 && !st.empty() && num[i] < st.top())
+            while( k > 0 and !st.empty() and st.top() > num[i] )
             {
                 k--;
                 st.pop();
             }
-            if (!st.empty() || num[i] != '0')
-                st.push(num[i]);
+
+            if( !st.empty() or num[i] != '0' )
+            {
+                st.push( num[i] );
+            }
         }
-        while (k > 0 && !st.empty())
+
+        while( k > 0 and !st.empty() )
         {
             k--;
             st.pop();
         }
-        if (st.empty())
+
+
+        if( st.empty() )
+        {
             return "0";
-        string ans = "";
-        while (!st.empty())
+        }
+
+        string ans;
+        
+        while( !st.empty() )
         {
             ans += st.top();
             st.pop();
         }
-        reverse(ans.begin(), ans.end());
+
+        reverse( begin(ans), end(ans) );
+
         return ans;
     }
 };
