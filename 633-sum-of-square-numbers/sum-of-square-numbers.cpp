053ad0aa7707946fab0308d2c6c29b693cae5af0
Aayush Anand <121629475+aayush751@users.noreply.github.com>
2024-04-25 10:52:12 +0530

    Time: 4 ms (49.62%) | Memory: 7.2 MB (20.37%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.cpp
+++ 633-sum-of-square-numbers/sum-of-square-numbers.cpp
@@ -1,7 +1,30 @@
 class Solution {
 public:
+    int getSqrt( int c )
+    {
+        int low = 0, high = c;
+        long long mid = low + ( high - low ) / 2;
+
+        int ans = -1;
+
+        while( low <= high )
+        {
+            if( mid * mid <= c )
+            {
+                ans = mid;
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+            mid = low + ( high - low ) / 2;
+        }
+
+        return ans;
+    }
     bool judgeSquareSum(int c) {
-        long long i = 0, j = sqrt(c);
+        long long i = 0, j = getSqrt(c);
 
         while( i <= j )
         {
