321f9852ce132eee7f1dc43e081e56b424a6db0e
Aayush Anand <121629475+aayush751@users.noreply.github.com>
2024-05-11 21:32:57 +0530

    Time: 20 ms (83.95%) | Memory: 15.6 MB (70.54%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,20 +1,39 @@
 class Solution {
 public:
-    int kthSmallest(vector<vector<int>>& m, int k) {
-        priority_queue< int > pq;
 
-        for(int i = 0; i < m.size(); i++ )
+    int countOfSmallerElement( vector<vector<int>>& matrix, int mid )
+    {
+        int count = 0;
+        for(int i = 0; i < matrix.size(); i++ )
         {
-            for(int j = 0; j < m.size(); j++ )
+            count += upper_bound( matrix[i].begin(), matrix[i].end(), mid ) - matrix[i].begin();
+        }
+        // ye function ye dega ki hmara assumed mid se kitna value chhota hai..taki pata lg sake kth smalles element
+        return count;
+    }
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        // ab binary search apply kr rhe hai bina extra space lekr bnane k liye
+        int n = matrix.size(), m = matrix.size();
+        int low = matrix[0][0], high = matrix[n-1][m-1];
+        int mid = low + ( high - low ) / 2;
+
+        int result = 0;
+        while( low <= high )
+        {
+            int count = countOfSmallerElement( matrix, mid );
+
+            if( count >= k )
             {
-                pq.push( m[i][j] );
-                if( pq.size() > k )
-                {
-                    pq.pop();
-                }
+                result = mid;
+                high = mid - 1;
             }
+            else
+            {
+                low = mid + 1;
+            }
+            mid = low + ( high - low ) / 2;
         }
 
-        return pq.top();
+        return result;
     }
 };
