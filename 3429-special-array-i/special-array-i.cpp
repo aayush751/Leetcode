974058420e100e8108354310876462fdf3279e62
Aayush Anand <121629475+aayush751@users.noreply.github.com>
2024-05-19 19:38:44 +0530

    Time: 4 ms (80.00%) | Memory: 26.8 MB (80.00%) - LeetSync

--- 3429-special-array-i/special-array-i.cpp
+++ 3429-special-array-i/special-array-i.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     bool isArraySpecial(vector<int>& nums) {
-        int n = nums.size();
-        if( n == 1 )
-        {
-            return 1;
-        }
+        
 
-        for(int i = 0; i < n - 1; i++ )
+        int n = nums.size();
+        for(int i = 1; i < n; i++ )
         {
-            if( nums[i] != nums[i + 1] and (nums[i] % 2 == 0 and nums[i + 1] % 2 == 1) or (nums[i] % 2 == 1 and nums[i + 1] % 2 == 0) )
-            {
-                continue;
-            }
-            else
+            if( nums[i] % 2 == nums[i - 1] % 2 )
             {
                 return false;
             }
