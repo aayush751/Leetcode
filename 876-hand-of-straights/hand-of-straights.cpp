ffd77be564dc9abc57ad4fffaef1bff1d64ae3d2
Aayush Anand <121629475+aayush751@users.noreply.github.com>
2024-06-07 08:08:40 +0530

    Time: 69 ms (29.94%) | Memory: 30.4 MB (78.21%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.cpp
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -1,68 +1,40 @@
 class Solution {
 public:
-    bool isNStraightHand(vector<int>& hand, int size) {
-        // sort( begin(hand), end(hand) );
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        
 
-        if( hand.size() % size != 0)
+        if( hand.size() % groupSize != 0 )
         {
             return false;
         }
 
         map< int, int > mp;
-        for(auto ele: hand )
+        for(auto i: hand )
         {
-            mp[ele]++;
+            mp[i]++;
         }
 
-        // int groupSizeCount = 0;
-        // int value = hand[0];
-        // int ans = 0;
-        // while(1)
-        // {
-        //     if( groupSizeCount == size )
-        //     {
-        //         value = hand[0];
-        //         groupSizeCount = 0;
-        //         ans++;
-        //     }
-
-        //     if( mp.find( value ) != mp.end() )
-        //     {
-        //         mp[value]--;
-        //         value++;
-        //         groupSizeCount++;
-        //     }
-        //     else
-        //     {
-        //         value++;
-        //     }
-
-        //     if( ans == size )
-        //     {
-        //         break;
-        //     }
-        // }
-
-        // return ans % size == 0;
-
-        for(auto it = mp.begin(); it != mp.end(); it++ )
+        for(auto [value, freq] : mp )
         {
-            int currValue = it -> first;
-            int count = it -> second;
-
-            if( count > 0 )
+            if( freq == 0 )
             {
-                for(int i = 0; i < size; i++ )
+                continue;
+            }
+
+            while( freq-- )
+            {
+                for(int i = value; i < value + groupSize; i++ )
                 {
-                    if( mp[currValue + i] < count )
+                    if( mp[i] == 0 )
                     {
                         return false;
                     }
 
-                    mp[currValue + i] -= count;
+                    mp[i]--;
                 }
             }
         }
+
         return true;
     }
 };
