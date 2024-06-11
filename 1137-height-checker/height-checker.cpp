4d0043e5999905d7b81bc46a49fbae36b742e997
Aayush Anand <121629475+aayush751@users.noreply.github.com>
2024-06-11 15:01:12 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (78.66%) - LeetSync

--- 1137-height-checker/height-checker.cpp
+++ 1137-height-checker/height-checker.cpp
@@ -1,21 +1,37 @@
 class Solution {
 public:
     int heightChecker(vector<int>& heights) {
-        vector< int > heightsCopy(heights);
+        // without using sorting function. 
+        // we will use counting sort
 
-        sort( begin(heights), end(heights) );
 
-        int ans = 0;
+        vector< int > count(101,0);
+
+        for( auto h: heights )
+        {
+            count[h]++;
+        }
+
+        int ans = 0, id = 1;
 
         for(int i = 0; i < heights.size(); i++ )
         {
-            if( heights[i] != heightsCopy[i] )
+            while( count[id] == 0 )
+            {
+                id++;
+            }
+
+            if( heights[i] == id )
+            {
+                count[id]--;
+            }
+            else
             {
                 ans++;
+                count[id]--;
             }
         }
 
         return ans;
     }
-
 };
