43ebce8b048dd987fb330b40fa5f5b87b3462264
Aayush Anand <121629475+aayush751@users.noreply.github.com>
2024-06-12 14:23:00 +0530

    Time: 61 ms (41.20%) | Memory: 53.3 MB (75.52%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
+        // sbse phle bina space wla approach lengein...
+        // esme kya krna hai jo valid number( negative number ) nhi hai usko n +1 se replace kr dena hai
 
         int n = nums.size();
 
-        for(int i = 0; i < nums.size(); i++ )
+        for(int i = 0; i < n; i++ )
         {
-            int element = nums[i];
-            
-
-            if( element >= 1 and element <= n )
+            if( nums[i] <= 0 )
             {
-                int chair = element - 1;
-                if( nums[chair] != element )
-                {
-                    swap( nums[chair], nums[i] );
-                    i--;
-                }
+                nums[i] = n + 1;
+            }
+        }
+
+        for( int i = 0; i < n; i++ )
+        {
+            if( abs( nums[i] ) <= n and nums[abs(nums[i]) - 1] > 0 )
+            {
+                nums[ abs(nums[i]) - 1 ] = nums[ abs(nums[i]) - 1 ] * -1;
             }
         }
 
         for(int i = 0; i < n; i++ )
         {
-            if( i + 1 != nums[i] )
+            if( nums[i] > 0 )
             {
                 return i + 1;
             }
