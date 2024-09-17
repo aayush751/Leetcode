79c83025ba944bf889dc7052b2587b35c8570d9d
Aayush Anand <121629475+aayush751@users.noreply.github.com>
2024-09-17 10:57:44 +0530

    Time: 6 ms (15.91%) | Memory: 9.8 MB (5.30%) - LeetSync

--- 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.cpp
+++ 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.cpp
@@ -1,58 +1,58 @@
 class Solution {
 public:
     vector<string> uncommonFromSentences(string s1, string s2) {
-        istringstream as1(s1);
-        istringstream as2(s2);
+        
+        istringstream as1( s1 );
+        istringstream as2( s2 );
 
-        string ans1, ans2;
-        unordered_map< string, int > mp, mp2;
+        string str1, str2;
 
         vector< string > v1, v2;
-        
-        while( as1 >> ans1 )
+        unordered_map< string, int > mps1, mps2;
+        while( as1 >> str1 )
         {
-            v1.push_back( ans1 );
+            v1.push_back( str1 );
         }
 
-        while( as2 >> ans2 )
+        while( as2 >> str2 )
         {
-            v2.push_back( ans2 );
+            v2.push_back( str2 );
         }
 
         for(auto i: v1 )
         {
-            mp[i]++;
+            mps1[i]++;
         }
 
         for(auto i: v2 )
         {
-            mp2[i]++;
+            mps2[i]++;
         }
 
         vector< string > ans;
-        for(int i = 0; i < v2.size(); i++ )
-        {
-            if( mp2[v2[i]] == 1 )
-            {
-                if( mp.find( v2[i] ) == mp.end() )
-                {
-                    ans.push_back( v2[i] );
-                }
-            }
-        }
 
         for(int i = 0; i < v1.size(); i++ )
         {
-            if( mp[v1[i]] == 1 )
+            if( mps1[v1[i]] == 1 )
             {
-                if( mp2.find( v1[i] ) == mp2.end() )
+                if( mps2.find( v1[i] ) == mps2.end() )
                 {
                     ans.push_back( v1[i] );
                 }
             }
         }
 
-        return ans;
+        for(int i = 0; i < v2.size(); i++ )
+        {
+            if( mps2[v2[i]] == 1 )
+            {
+                if( mps1.find( v2[i] ) == mps1.end() )
+                {
+                    ans.push_back( v2[i] );
+                }
+            }
+        }
 
+        return ans;
     }
 };
