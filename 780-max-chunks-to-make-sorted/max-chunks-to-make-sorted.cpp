7b77f6dec75613059e0cdf60acd8e9398f315416
Aayush Anand <121629475+aayush751@users.noreply.github.com>
2024-12-19 22:58:17 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (20.72%) - LeetSync

---
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        int chunks = 0;
+        int maxVal = 0;
+
+        for(int i = 0; i < arr.size(); i++ )
+        {
+            maxVal = max( maxVal, arr[i] );
+
+            if( maxVal == i )
+            {
+                chunks++;
+            }
+        }                
+
+        return chunks;              
+    }
+};
